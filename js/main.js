704ce94d9806d15c496db942266d7a5597e036f4
somej-123 <somej@naver.com>
2021-04-13 22:48:55 +0900

    mobile메인 슬라이드1 완성

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var aa = true;
+    var bb = true;
     slideStart();
+    slideStart2();
     $("#hamburger").click(function(){
         if(aa == true){
             aa = false;
@@ -22,13 +24,43 @@ $(document).ready(function(){
             // $(".SearchBox").fadeOut(1000);
         }
     });
-    //슬라이드 속도
+    $("#M_hamburger").click(function(){
+        if(bb == true){
+            bb = false;
+            $("#M_hamburger").addClass("rotate");
+            $("#M_hamburger").attr("src","img/X.png");
+            $("#M_hamburger").removeClass("back");
+            $("#M_SearchBox").stop().animate({
+                "margin-left":0+"%"
+            },1000);
+            // $(".SearchBox").fadeIn(1000);
+        }else{
+            bb = true;
+            $("#M_hamburger").addClass("back");
+            $("#M_hamburger").attr("src","img/menu.png");
+            $("#M_hamburger").removeClass("rotate");
+            $("#M_SearchBox").stop().animate({
+                "margin-left":-100+"%"
+            },1000);
+            // $(".SearchBox").fadeOut(1000);
+        }
+    });
+        //슬라이드 속도
         function slideStart(){
             var myCarousel = document.querySelector('#myCarousel')
             var carousel = new bootstrap.Carousel(myCarousel, {
-            interval: 100000,
+            interval: 10000,
             wrap:true,
-        })
-    }
+            });
+        }
+        //모바일 슬라이드 속도
+        function slideStart2(){
+            var myCarousel = document.querySelector('#mobile_slide')
+            var carousel = new bootstrap.Carousel(myCarousel, {
+            interval: 10000,
+            wrap:true,
+            });
+        }
+
     
 });
