2029ba42660b93751850edbfb5b078ffe390010e
somej-123 <somej@naver.com>
2021-04-22 00:19:05 +0900

    노트 메인 모바일 디자인 완료

---
+++ js/Mobile_menu.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $(".nav_plus").click(function(){
+        $("#M_aside_menu").animate({
+            "right":0
+        },300);
+        $(".nav_plus").hide();
+        $("#sub_page").addClass("contentsBackground");
+        $("footer").addClass("contentsBackground");
+    });
+
+    $("#close").click(function(){
+        $("#M_aside_menu").animate({
+            "right":-300
+        },300);
+        $(".nav_plus").show();
+        $("#sub_page").removeClass("contentsBackground");
+        $("footer").removeClass("contentsBackground");
+    })
+})
