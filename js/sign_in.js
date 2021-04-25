921806dae0e4f2881a10aff0db7225fa9a6f319c
somej-123 <somej@naver.com>
2021-04-25 22:22:09 +0900

    html만 적용 예정

--- js/sign_in.js
+++ js/sign_in.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
     var ID_confirm = false
 
     $("#sign_in input").on("input",function(){
+        if($("#input_password").val() == $("#input_password2").val() && $("#input_password").val() != "" && $("#input_password2").val() != ""){
+            $("#password_confirm_check_Y").css("display","block");
+            $("#password_confirm_check_N").css("display","none");
+        }else{
+            $("#password_confirm_check_Y").css("display","none");
+            $("#password_confirm_check_N").css("display","block");
+        }
+
         if($("#input_ID").val() == "" || $("#input_ID").val() == null){
             hoverEvt1();
         }
@@ -35,6 +43,9 @@ $(document).ready(function(){
         else if($("#input_password2").val() == "" || $("#input_password2").val() == null){
             alert("비밀번호 확인을 입력해주세요");
             return;
+        }
+        else if($("#input_password").val() != $("#input_password2").val()){
+            alert("비밀번호를 다시 확인해주세요")
         }
         else if(ID_confirm == false){
             alert("아이디 중복 확인을 해주세요");
